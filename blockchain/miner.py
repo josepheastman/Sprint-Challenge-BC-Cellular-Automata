7cb6ab3dad0581fd6ebdffc4f35573159d1cf3f9
Joseph <josephryan96@gmail.com>
2019-08-18 20:41:53 -0700

    miner.py wip

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,6 +24,8 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -36,9 +38,13 @@ def valid_proof(last_hash, proof):
 
     IE:  last_hash: ...999123456, new hash 123456888...
     """
-
-    # TODO: Your code here!
-    pass
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    beg = guess_hash[0:6]
+    if beg == "000000":
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
